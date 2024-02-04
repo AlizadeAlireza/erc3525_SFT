4bc464830833d321904d08ab9cb67c368b0539a6
alireza alizadeh <alireza.aa9700@gmail.com>
2024-02-04 13:13:58 +0330

    add mint variables and setNewTokenPrice function

--- contracts/SFT.sol
+++ contracts/SFT.sol
@@ -10,12 +10,14 @@ import "@solvprotocol/erc-3525/ERC3525.sol";
 
 contract SFT is Ownable, ERC3525 {
     using Strings for uint256;
-    // address private owner;
-    uint public tokenValue = 0;
+
+    uint public _tokenPrice = 0;
+    uint public _slot = 0;
+    uint public _tokenValue = 1;
 
     constructor() Ownable(msg.sender) ERC3525("alireza", "AZK", 18) {}
 
-    // function setNewTokenValue(uint _newTokenValue) public onlyOwner {
-    //     tokenValue = _newTokenValue;
-    // }
+    function setNewTokenPrice(uint _newTokenPrice) public onlyOwner {
+        _tokenPrice = _newTokenPrice;
+    }
 }
